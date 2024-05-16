565fb2ab79e8806e4251ab47a62aa076aee7813f
YongenChen <yongen.chen@sjsu.edu>
2024-05-15 20:58:39 -0700

    check_anchor_order not needed for FasterRCNN, it's for yolov7

--- DeepDataMiningLearning/detection/modules/anchor.py
+++ DeepDataMiningLearning/detection/modules/anchor.py
@@ -9,18 +9,6 @@ from tqdm import tqdm
 
 #from utils.general import colorstr
 
-
-def check_anchor_order(m):
-    # Check anchor order against stride order for YOLO Detect() module m, and correct if necessary
-    a = m.anchor_grid.prod(-1).view(-1)  # anchor area
-    da = a[-1] - a[0]  # delta a
-    ds = m.stride[-1] - m.stride[0]  # delta s
-    if da.sign() != ds.sign():  # same order
-        print('Reversing anchor order')
-        m.anchors[:] = m.anchors.flip(0)
-        m.anchor_grid[:] = m.anchor_grid.flip(0)
-
-
 def check_anchors(dataset, model, thr=4.0, imgsz=640):
     # Check anchor fit to data, recompute if necessary
     prefix = 'autoanchor: ' #colorstr('autoanchor: ')
